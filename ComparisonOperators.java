252c38f8ff4d801db944bd994a054afab8444f25
Shivang singh <shivangofficial04@gmail.com>
2026-08-28 12:38:31 +0530

    Add ComparisonOperators  code

---
+++ ComparisonOperators.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ComparisonOperators {
+    
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int a = sc.nextInt();
+
+        System.out.println(a > 8);
+    }
+}
+    
+
