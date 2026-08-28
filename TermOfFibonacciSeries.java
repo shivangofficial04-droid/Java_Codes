8f1bad156c3f79aeaf19d37c77d0f8e80ebed65d
Shivang singh <shivangofficial04@gmail.com>
2026-08-28 14:02:47 +0530

    Add TermOfFibonacciSeries

---
+++ TermOfFibonacciSeries.java
@@ -0,0 +1,19 @@
+public class TermOfFibonacciSeries {
+
+    static int fibonacci(int n) {
+
+        if (n == 1) {
+            return 0;
+        }
+
+        if (n == 2) {
+            return 1;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(7));
+    }
+}
